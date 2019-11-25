c46279c1aacb353c8f9679f6dedbe28d4e48e2ae
alan liang @vapps <alan.liang@xcerra.com>
2019-11-25 20:24:36 +0800

    rename NegativeTest to NegativeABTest; add more test
    	modified:   unit-test.cpp

--- unit-test.cpp
+++ unit-test.cpp
@@ -11,12 +11,52 @@ TEST(ExpFitTest, PositiveTest)
   EXPECT_NEAR(std::get<2>(result), 0, 1e-6);
 }
 
-TEST(ExpFitTest, NegativeTest)
+TEST(ExpFitTest, NegativeABTest)
 {
   ExpFit expfit;
   expfit.SetTest(5, -1, -1.0, 0);
   auto result = expfit.GetResult();
   EXPECT_NEAR(std::get<0>(result), -1, 1e-6);
   EXPECT_NEAR(std::get<1>(result), -1, 1e-6);
+  EXPECT_NEAR(std::get<2>(result), 0, 1e-6);
+}
+
+TEST(ExpFitTest, NegativeATest)
+{
+  ExpFit expfit;
+  expfit.SetTest(5, -1, 1.0, 0);
+  auto result = expfit.GetResult();
+  EXPECT_NEAR(std::get<0>(result), -1, 1e-6);
+  EXPECT_NEAR(std::get<1>(result), 1, 1e-6);
+  EXPECT_NEAR(std::get<2>(result), 0, 1e-6);
+}
+
+TEST(ExpFitTest, NegativeBTest)
+{
+  ExpFit expfit;
+  expfit.SetTest(5, 1, -1.0, 0);
+  auto result = expfit.GetResult();
+  EXPECT_NEAR(std::get<0>(result), 1, 1e-6);
+  EXPECT_NEAR(std::get<1>(result), -1, 1e-6);
+  EXPECT_NEAR(std::get<2>(result), 0, 1e-6);
+}
+
+TEST(ExpFitTest, PositiveMorePointsTest)
+{
+  ExpFit expfit;
+  expfit.SetTest(50, 1, 1, 0);
+  auto result = expfit.GetResult();
+  EXPECT_NEAR(std::get<0>(result), 1, 1e-6);
+  EXPECT_NEAR(std::get<1>(result), 1, 1e-6);
+  EXPECT_NEAR(std::get<2>(result), 0, 1e-6);
+}
+
+TEST(ExpFitTest, NegativeMorePointsTest)
+{
+  ExpFit expfit;
+  expfit.SetTest(50, -1, -1.0, 0);
+  auto result = expfit.GetResult();
+  EXPECT_NEAR(std::get<0>(result), -1, 0.404);
+  EXPECT_NEAR(std::get<1>(result), -1, 0.034);
   EXPECT_NEAR(std::get<2>(result), 0, 1e-6);
 }
